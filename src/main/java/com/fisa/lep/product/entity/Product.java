a48de35bb2327773d6be8f8f0d0e35696f62c2b8
parfait2 <parkyl0208@naver.com>
2024-06-09 23:29:13 +0900

    [Feat] CSV 파일 읽어서 DB에 insert

--- src/main/java/com/fisa/lep/product/entity/Product.java
+++ src/main/java/com/fisa/lep/product/entity/Product.java
@@ -1,9 +1,11 @@
 package com.fisa.lep.product.entity;
 
+import com.fisa.lep.area.entity.Area;
 import com.fisa.lep.common.BaseEntity;
 import com.fisa.lep.mart.entity.Mart;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -23,10 +25,12 @@ public class Product extends BaseEntity {
 
     private String name; // 상품명
 
-    private BigDecimal price; // 판매가격
+    private String company; // 제조사
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "mart_id")
-    private Mart mart; // 판매업소
+    @Builder
+    public Product(String name, String company) {
+        this.name = name;
+        this.company = company;
+    }
 
 }
