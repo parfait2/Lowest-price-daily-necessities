b3c575b4906d3cf3fa848dbdb8c22f1f47669181
kim-young-hyun <62740034+kim-young-hyun@users.noreply.github.com>
2024-06-11 16:20:49 +0900

    [Fix]

---
+++ src/main/java/com/fisa/lep/mart/entity/Brand.java
@@ -0,0 +1,11 @@
+package com.fisa.lep.mart.entity;
+
+public enum Brand {
+    NH, // 농협
+    CU, // BGF리테일
+    GS, // GS리테일
+    LOTTE, // 롯데
+    SSG, // 이마트, 신세계
+    HYUNDAI, // 현대
+    NONE // 그 외
+}
--- src/main/java/com/fisa/lep/mart/entity/Mart.java
+++ src/main/java/com/fisa/lep/mart/entity/Mart.java
@@ -4,16 +4,15 @@ import com.fisa.lep.area.entity.Area;
 import com.fisa.lep.common.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-
 @RequiredArgsConstructor
 @AllArgsConstructor
 @Getter
 @Table(name = "mart")
 @Entity
 public class Mart extends BaseEntity {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "mart_id")
@@ -22,9 +21,18 @@ public class Mart extends BaseEntity {
     private String name;
 
     private String brand;
+    private Brand brand;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "area_id")
     private Area area;
+
+    @Builder
+    public Mart(String name, Area area) {
+    public Mart(String name, Brand brand, Area area) {
+        this.name = name;
+        this.brand = brand;
+        this.area = area;
+    }
 
 }
